107b3b322b5eb0e58f7900bea0637022f3590a21
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-02-01 14:11:56 -0700

    Add files via upload

---
+++ piglat2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc,char *argv[])
+{
+
+string inptstr;
+cout << "(F)ile or (B)ash input?"<<endl;
+cin >> type;
+return 0;
+}
+
+void fread()
+{
+
+}
