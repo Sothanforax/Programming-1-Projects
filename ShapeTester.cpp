c66e225c463dc822a1395e9efacdb95087a71cfa
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-02-21 13:51:21 -0700

    Add files via upload

---
+++ ShapeTester.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.
+using namespace std;
+int main ()
+{
+string choice;
+cout<<"\nEnter a Shape to compute";
+cout<<"\nYour choices are: Box,Sphere,Pyramid";
+cout<<"\n>";
+getline (cin,choice);
+
+}
+
+class Box
+{
+int l,w,h;
+}
+
+class Sphere
+{
+
+}
+
+class Pyramid
+{
+
+}
---
+++ bottles.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int x = 99; x!=0 ;x--)
+    {
+        cout<<"\n"<<x<<" Bottles of generic fluid on the wall!";
+    }
+cout<<"\n0th Bottle of generic fluid on the wall!";
+cout<<"\n";
+    return 0;
+}
