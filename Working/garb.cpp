0ed9055cfdf3b5a79d8c837f86c4c8e2d84eba57
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-02-28 14:58:55 -0700

    Add files via upload

---
+++ Working/garb.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char sos;
+cout<<"Commence Fap?";
+cout<<"\nY/N"<<endl;
+cout<<">";
+cin>>sos;
+while (sos == 'y')
+{
+cout<<"Fap";
+}
+return 0;
+}
