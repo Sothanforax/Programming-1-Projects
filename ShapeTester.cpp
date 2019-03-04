6cbd6a6082d419ef968f874d12312b9c25b82e2e
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-03-04 14:10:49 -0700

    Update ShapeTester.cpp

--- ShapeTester.cpp
+++ ShapeTester.cpp
@@ -1,40 +1,69 @@
 #include<iostream>
 #include<string>
-#include<cmath>
 using namespace std;
 class Box
 {
 private:
 double l,w,h;
+double result;
 public:
-void vol(int inptvol[3],int outvol)
+void getvol(double inptvol1,double inptvol2,double inptvol3)
 {
-l = inptvol[0];
-w = inptvol[1];
-h = inptvol[2];
-outvol = l*w*h;
+l = inptvol1;
+w = inptvol2;
+h = inptvol3;
+result=l*w*h;
+cout<<"\nVolume:"<<result;
 }
-void sa(int outsa)
+void getsa()
 {
-outsa = l*l*6;
+result = l*l*6;
+cout<<"\nSurface Area:" << result;
 }
-
 };
 class Sphere
 {
 private:
 double r,d;
+double result;
+double pi = 3.141592654;
+//Texas Instruments pi
+int charconv;
+public:
+char raddiam;
+void getvol(double inptvol)
+{
+    if (raddiam == 'r') 
+    {
+     charconv = 2;   
+    }
+    else if (raddiam == 'd')
+    {
+        charconv = 1;
+    }
+    switch (charconv)
+    {
+        case 1:
+        d = inptvol;
+        break;
+        case 2:
+        r = inptvol;
+        result = 4/3*pi*r*r*r;
+        cout<<"\nVolume:"<<result;
+        break;
+}
+}
 };
 class Pyramid
 {
 private:
+double result;
 };
 int main ()
 {
 string choice;
 int choiceint;
-double result;
-int input[3];
+double input[3];
 Box box;
 Sphere sph;
 Pyramid pyr;
@@ -49,7 +78,7 @@ if (choice == "box" || choice == "Box" || choice == "b")
 {
     choiceint = 1;
 }
-else if (choice == "sphere" || choice == "Sphere")
+else if (choice == "sphere" || choice == "Sphere" || choice == "s")
 {
     choiceint = 2;
 }
@@ -66,18 +95,19 @@ switch(choiceint)
     case 1:
     cout<<"\nLength?"<<endl;
     cin>>input[0];
-    cout<<"\nWidth?"<<endl;
+    cout<<"Width?"<<endl;
     cin>>input[1];
-    cout<<"\nHeight?"<<endl;
+    cout<<"Height?"<<endl;
     cin>>input[2];
-box.vol(input[0],input[1],input[2]);
-cout<<"\nVolume:"<<result<<endl;
-box.sa();
-cout<<"\nSurface Area:"<<result<<endl;
+box.getvol(input[0],input[1],input[2]);
+box.getsa();
     break;
 
     case 2:
 cout<<"\n(R)adius or (D)iameter?"<<endl;
+cin>>sph.raddiam;
+cout<<"\nRadius/Diameter?"<<endl;
+cin>>input[0];
     break;
 
     case 3:
