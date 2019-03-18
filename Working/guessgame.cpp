211d3016126fc6f3b46fe8defdb73591e71078c6
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-03-18 13:55:00 -0600

    Update guessgame.cpp

--- Working/guessgame.cpp
+++ Working/guessgame.cpp
@@ -4,24 +4,33 @@
 using namespace std;
 int main ()
 {
+bool trybit = true;
+int trycount = 0;
 int ingss;
 srand (time(NULL));
 int randnum = rand()% 100;
 cout<<"\nWelcome to the number guessing game!";
+while (trybit == true)
+{
 cout<<"\nEnter a random number from 0-100 Below";
 cout<<"\n>";
 cin>>ingss;
 if (ingss == randnum)
 {
-cout<<"\nYour guessed number "<<ingss<<" is equal to "<<randnum<<" Congrats!";
+cout<<"\nYour guessed number "<<ingss<<" is equal to "<<randnum<<" Congrats!"<<endl;
+cout<<"And it took you "<<trycount<<" Tries!"<<endl;
+trybit = false;
 }
 else if (ingss < randnum)
 {
-cout<<"\nOh! Your guess "<<ingss<<" Is Too Low compared to "<<randnum<<" please try again";
+cout<<"\nOh! Your guess "<<ingss<<" Is Too Low,please try again";
+trycount++;
 }
 else if (ingss > randnum)
 {
-cout<<"\nOh! your guess "<<ingss<<" is too high compared to "<<randnum<<" please try again";
+cout<<"\nOh! your guess "<<ingss<<" is too high,please try again";
+trycount++;
+}
 }
 return 0;
 }
