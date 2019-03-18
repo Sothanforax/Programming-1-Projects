8f5ee0f96ebbe9a941e88bbb7c4e467f49c0ef2a
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-03-18 13:41:58 -0600

    Update and rename Broken/guessgame.cpp to Working/guessgame.cpp

--- Working/guessgame.cpp
+++ Working/guessgame.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
-#define RAND_MAX 100
 using namespace std;
 int main ()
 {
 int ingss;
-srand (time(0));
-int randnum = rand();
+srand (time(NULL));
+int randnum = rand()% 100;
 cout<<"\nWelcome to the number guessing game!";
 cout<<"\nEnter a random number from 0-100 Below";
 cout<<"\n>";
 cin>>ingss;
 if (ingss == randnum)
 {
-cout<<"\nYour guessed number" <<ingss<< "is equal to" <<randnum<< "Congrats";
+cout<<"\nYour guessed number "<<ingss<<" is equal to "<<randnum<<" Congrats!";
 }
 else if (ingss < randnum)
 {
-cout<<"\nOh your guess is too low please try again";
-cout<<"\n r:"<<randnum<<endl;;
+cout<<"\nOh! Your guess "<<ingss<<" Is Too Low compared to "<<randnum<<" please try again";
 }
 else if (ingss > randnum)
 {
-cout<<"\nOh your guess is too high please try again";
-cout<<"\n r:"<<randnum<<endl;
+cout<<"\nOh! your guess "<<ingss<<" is too high compared to "<<randnum<<" please try again";
 }
 return 0;
 }
